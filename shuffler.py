dc7efdc1d5c4fde499fedf9962e99679f54e2806
Tobias Ruiz Moreno <tobias.ruizmoreno@gmail.com>
2018-12-04 19:04:11 -0300

    Cambio de encoding a "utf-8"

--- shuffler.py
+++ shuffler.py
@@ -5,10 +5,10 @@ import os
 if __name__ == '__main__':
     lines = []
     for filename in os.listdir(sys.argv[1]):
-       inputfile = open(os.path.join(sys.argv[1], filename), "r")
+       inputfile = open(os.path.join(sys.argv[1], filename), "r",  encoding = 'utf8')
        lines.extend(inputfile.readlines())
        inputfile.close()
     lines = sorted(lines)
-    outputfile = open(sys.argv[2], "w")
+    outputfile = open(sys.argv[2], "w",   encoding = 'utf8')
     outputfile.writelines(lines)
     outputfile.close()
